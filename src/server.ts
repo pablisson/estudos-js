898ed18124e7a7c414107e5b8641e6336439b418
Páblisson <pablissonaraujo@gmail.com>
2021-03-08 15:26:58 -0300

    Projeto inicializando com a rota principal funionando.

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import express, { response } from 'express';
+
+const app = express();
+
+/**
+ * Nesse caso estamos criando as rotas, essa rota que será acessada via metodo get será encontrada na raiz
+ * http://localhost/
+ */
+app.get('/', (request, response) => {
+  return response.json({ message: "Hello World" });
+})
+
+app.listen(3333, () => {
+  console.log('Servidor funcionando');
+})
