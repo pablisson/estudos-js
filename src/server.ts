1ee8571c58a510816784072aa403c82ec2e593a6
Pablisson <pablissonaraujo@gmail.com>
2021-03-09 14:58:19 -0300

    primeiro teste com as rotas.(debug funcionando)

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,22 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
+
+app.use(express.json());
+app.use(routes);
+
 
 /**
  * Nesse caso estamos criando as rotas, essa rota que será acessada via metodo get será encontrada na raiz
  * http://localhost/
  */
+
+/*
 app.get('/', (request, response) => {
   return response.json({ message: "Hello World" });
 })
-
+*/
 app.listen(3333, () => {
   console.log('Servidor funcionando');
 })
