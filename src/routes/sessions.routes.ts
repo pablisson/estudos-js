8f51cd9eb46945b7b02776dab24d5c966585ee54
Pablisson <pablissonaraujo@gmail.com>
2021-03-12 18:35:55 -0300

    Autenticação do Usuario.

---
+++ src/routes/sessions.routes.ts
@@ -0,0 +1,24 @@
+ import { Router } from 'express';
+
+ import AuthenticateUserService from '../services/AuthenticateUserService';
+ 
+ const sessionRouter = Router();
+ 
+ sessionRouter.post('/', async (request, response) => {
+   try {
+      const{ email, password } = request.body;
+
+      const authenticateUser = new AuthenticateUserService();
+
+      const { user } = await authenticateUser.execute({
+        email,
+        password,
+      });
+
+      return response.json({ user });
+   } catch (error) {
+     return response.status(400).json({ error: error.message });
+   }
+ });
+ 
+ export default sessionRouter; 
