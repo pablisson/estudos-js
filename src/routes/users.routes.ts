e67a4c1de95680a3b61048049abbeda573420b66
Pablisson <pablissonaraujo@gmail.com>
2021-03-15 18:54:31 -0300

    Upload de arquivos de imagem.

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -2,11 +2,16 @@
  * ROTA: Recebe a requisição, chama outro arquivo, devolve uma resposta
  */
  import { Router } from 'express';
+ import multer from 'multer';
+ import uploadConfig from '../config/upload';
 
  import CreateUserService from '../services/CreateUserService';
- 
+ import ensureAuthenticated from '../middlewares/ensureAuthenticated';
+
  const usersRouter = Router();
- 
+ const upload = multer(uploadConfig);
+
+
  usersRouter.post('/', async (request, response) => {
    try {
      
@@ -28,5 +33,16 @@
      return response.status(400).json({ error: error.message });
    }
  });
+
+ usersRouter.patch(
+    '/avatar',
+    ensureAuthenticated, 
+    upload.single('avatar'),
+    async (request, response) => 
+    {
+      console.log(request.file);
+      return response.json({ ok: true });
+    } 
+ );
  
  export default usersRouter; 
